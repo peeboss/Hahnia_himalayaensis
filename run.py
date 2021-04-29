19df6c94696d78c952da10718f498e75c9f2712c
peeboss <tgch553@gmail.com>
2021-04-29 16:59:58 +0800

    find id by regular expresssion

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import requests
 import os
+import re
 from bs4 import BeautifulSoup
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.49'}
 def getlinks(url):
 	rep =   requests.get(url,headers=headers)
 	bs  =   BeautifulSoup(rep.text,'html.parser')
 	sound_list  =   bs.select('div.sound-list._is a')
+	#/renwen/15158548/207024308
 	for link in sound_list:
-		print(link['href'])
+		print(re.match(r'/(\w+)/(\d+)/(\d+)',link['href'])[3])
 if __name__ ==  '__main__':
     getlinks('https://www.ximalaya.com/renwenjp/15158548/')
     
