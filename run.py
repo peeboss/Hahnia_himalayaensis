0e677bd6b818fae78b15942552c3378eab6ba0c9
peeboss <tgch553@gmail.com>
2021-05-01 11:04:18 +0800

    ready to get download link

--- run.py
+++ run.py
@@ -9,7 +9,13 @@ def getlinks(url):
 	sound_list  =   bs.select('div.sound-list._is a')
 	#/renwen/15158548/207024308
 	for link in sound_list:
-		print(re.match(r'/(\w+)/(\d+)/(\d+)',link['href'])[3])
+		#正则表达式从超链接中读出音频id号
+		#例如/renwen/15158548/207024308中的207024308
+		parse('https://www.ximalaya.com/revision/play/v1/audio?id='+re.match(r'/(\w+)/(\d+)/(\d+)',link['href'])[3]+'&ptype=1')
+def parse(url):
+	rep=	requests.get(url,headers=headers)
+	json=	rep.json()
+	print(json['data']['src'])
+
 if __name__ ==  '__main__':
     getlinks('https://www.ximalaya.com/renwenjp/15158548/')
-    
